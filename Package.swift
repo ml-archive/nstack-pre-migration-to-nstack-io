15150f40d084179aeb2e35ab2fc55d6f10bbd248
Rasmus Ebbesen <re@nodes.dk>
2018-09-27 12:17:47 +0200

    added leaf

--- Package.swift
+++ Package.swift
@@ -5,5 +5,6 @@ let package = Package(
         dependencies: [
                 .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
                 .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/vapor/leaf.git", majorVersion: 2)
         ]
 )
