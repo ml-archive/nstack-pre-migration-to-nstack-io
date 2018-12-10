82dad586000cd74e9c93cfc3a1df7b7e0751d092
Heidi Puk Hermann <heih@nodesagency.com>
2018-12-10 11:30:58 +0100

    Add leaf as an dependency

--- Package.swift
+++ Package.swift
@@ -8,9 +8,10 @@ let package = Package(
         ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.1")
         ],
     targets: [
-        .target(name: "NStack", dependencies: ["Vapor"]),
+        .target(name: "NStack", dependencies: ["Vapor", "Leaf"]),
         .testTarget(name: "NStackTests", dependencies: ["NStack"]),
         ]
 )
