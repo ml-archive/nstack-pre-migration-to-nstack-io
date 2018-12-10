5132e1777a3aa87522ff845b816a351a889ee623
Heidi Puk Hermann <heih@nodesagency.com>
2018-12-10 11:05:37 +0100

    Remove dependency on Sugar

--- Package.swift
+++ Package.swift
@@ -8,11 +8,9 @@ let package = Package(
         ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        // required for MutableLeafTagConfig
-        .package(url: "https://github.com/nodes-vapor/sugar.git", from: "3.0.0-beta")
         ],
     targets: [
-        .target(name: "NStack", dependencies: ["Vapor", "Sugar"]),
+        .target(name: "NStack", dependencies: ["Vapor"]),
         .testTarget(name: "NStackTests", dependencies: ["NStack"]),
         ]
 )
